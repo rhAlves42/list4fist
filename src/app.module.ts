d94f37128199fa381ac39a1d2bd03511a4f7d6f8
Ricardo Henrique <ricardo@wcore.com.br>
2020-02-14 20:48:48 -0300

    adicionando mongo, preciso formatar o ubuntu

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { ExercicesController } from './exercices/exercices.controller';
+import { ExercicesModule } from './exercices/exercices.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    ExercicesModule, 
+    GraphQLModule.forRoot({
+    autoSchemaFile: 'schema.gql',
+   }),
+   MongooseModule.forRoot(
+     'mongodb://localhost/nestgraphql'
+    ),
+  ],
+  controllers: [AppController, ExercicesController],
   providers: [AppService],
 })
 export class AppModule {}
