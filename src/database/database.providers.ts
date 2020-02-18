14f8aa1d89819d00c29931a3a5eade9a9bc751b3
Ricardo Henrique <tio.rick.rh@gmail.com>
2020-02-18 14:56:16 -0300

    setando db providers

---
+++ src/database/database.providers.ts
@@ -0,0 +1,12 @@
+import * as mongoose from 'mongoose';
+import { KEYS } from '../config';
+
+export const DATABASE_CONNECTION = 'DATABASE_CONNECTION';
+
+export const databaseProviders = [
+    {
+        provide: DATABASE_CONNECTION,
+        useFactory: (): Promise<any> =>
+            mongoose.connect(KEYS.mongo_url, {useNewUrlParser: true}),
+    }
+];
